169978c1f9d9d4e41cb90a7898b84bec41c2862c
[Pratikshap123] <pratikshajpatil2001@gmail.com>
2023-06-21 17:45:30 +0530

    change

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -120,7 +120,7 @@ namespace DotNetApiDemo.Controllers
                 {
                     return Ok("Post has been deleted.");
                 }
-                return BadRequest("Post delete failed.");
+                return BadRequest("commit.");
             }
             catch (Exception ex)
             {
